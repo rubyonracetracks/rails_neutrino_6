1e5deb4f87079135fe34acb8866a64becbd8831d
Jason Hsu <jhsu@midwestmortgageanalytics.com>
2021-06-10 15:49:31 -0500

    Run docker/build at end of start-docker.sh

--- start-docker.sh
+++ start-docker.sh
@@ -2,3 +2,11 @@
 set -e
 
 docker run -i -t --rm -v ${PWD}:/home/winner/neutrino registry.gitlab.com/rubyonracetracks/docker-debian-bullseye-rvm-rails-neutrino6 /home/winner/neutrino/start-build-rails
+
+APP_NAME=`cat tmp/app_name.txt`
+DIR_APP=$PWD/$APP_NAME
+cd $DIR_APP && docker/build
+echo '**********************************'
+echo 'Your new Rails app has been built!'
+echo 'Path:'
+echo "$DIR_APP"
