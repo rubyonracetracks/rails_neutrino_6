e619b433564809b402149c4ee9fb2c9cdb193ee1
Jason Hsu <jhsu802701@jasonhsu.com>
2021-06-18 18:00:13 -0500

    build new app in start-docker.sh

--- start-docker.sh
+++ start-docker.sh
@@ -16,6 +16,35 @@ GIT_NAME="$(git config user.name)"
 echo "$GIT_EMAIL" > tmp/git_email.txt
 echo "$GIT_NAME" > tmp/git_name.txt
 
+echo '+++++++++++++++++++++++++++'
+echo 'BEGIN: docker-compose build'
+echo '+++++++++++++++++++++++++++'
 docker-compose build
+echo '+++++++++++++++++++++++++'
+echo 'END: docker-compose build'
+echo '+++++++++++++++++++++++++'
 
+echo '#################################################'
+echo 'BEGIN: docker-compose run web bash build-rails.sh'
+echo '#################################################'
 docker-compose run web bash build-rails.sh
+echo '###############################################'
+echo 'END: docker-compose run web bash build-rails.sh'
+echo '###############################################'
+
+
+APP_NAME=`cat tmp/app_name.txt`
+echo '#######'
+echo 'NEW APP'
+echo 'BEGIN: docker/build'
+echo '###################'
+cd $APP_NAME && docker/build
+echo '#######'
+echo 'NEW APP'
+echo 'END: docker/build'
+echo '#################'
+
+echo '**********************************'
+echo 'Your new Rails app has been built!'
+echo 'Path:'
+echo "$PWD/$APP_NAME"
