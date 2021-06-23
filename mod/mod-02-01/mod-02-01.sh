79759e471cca5d7fbc9cdc9fa79e031af2f21528
Jason Hsu <jhsu802701@jasonhsu.com>
2021-06-23 21:45:43 +0000

    docker/cop works

--- mod/mod-02-01/mod-02-01.sh
+++ mod/mod-02-01/mod-02-01.sh
@@ -2,7 +2,8 @@
 set -e
 
 # AGENDA:
-# * Add the docker/sandbox script for running the Rails sandbox environment (WICHTIG/LEGACY)
+# * Add RuboCop
+# * Add docker/cop script (WICHTIG/LEGACY: if necessary)
 
 echo '############'
 echo 'UNIT 2: LINT'
@@ -12,6 +13,9 @@ echo '################################'
 echo 'Unit 2 Chapter 1: Adding RuboCop'
 echo '################################'
 
+echo 'Adding docker/cop'
+mv mod-02-01-docker_cop docker/cop
+chmod +x docker/cop
 ruby mod-02-01.rb
 
 git add .
