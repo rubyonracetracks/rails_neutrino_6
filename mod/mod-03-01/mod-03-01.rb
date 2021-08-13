e22054cf52c2d2c0b4307d6b2865f6bb51e29a48
Jason Hsu <jhsu802701@jasonhsu.com>
2021-08-13 06:07:02 +0000

    Enable brakeman in docker/git_check

--- mod/mod-03-01/mod-03-01.rb
+++ mod/mod-03-01/mod-03-01.rb
@@ -12,3 +12,7 @@ InsertFromFile.add_end('mod-03-01-Gemfile.txt', 'Gemfile')
 puts 'Enabling docker/brakeman in docker/test_code'
 StringInFile.replace('# docker/brakeman', 'docker/brakeman','docker/test_code')
 system('chmod +x docker/test_code')
+
+puts 'Enabling docker/brakeman in docker/git_check'
+StringInFile.replace('# docker/brakeman', 'docker/brakeman','docker/git_check')
+system('chmod +x docker/git_check')
